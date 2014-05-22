ad91bb38108982fbc580f31d546c54439ca69834
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-22 14:31:13 -0700

    new feature

--- test.rb
+++ test.rb
@@ -1,4 +1,3 @@
 puts "Hello World"
 puts "hashtag yoloswag"
-puts "This is my change that will cause a merge conflict"
-puts "this is my fourth branch change blah blah blah	"
+	puts FEATURE BRANCH CHANGE!! 
